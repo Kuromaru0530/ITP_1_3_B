aa06df67a332d35c41f23beb34eb4338096dd960
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 15:54:55 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP_1_3_B/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ITP_1_3_B
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int Count = 0;
+            while (true)
+            {
+                int x = int.Parse(Console.ReadLine());
+                
+                Count++;
+                Console.WriteLine($"Case {Count}: {x}");
+                if (x == 0) break;
+            } 
+
+        }
+    }
+}
